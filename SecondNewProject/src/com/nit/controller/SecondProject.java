b3a054d794ceeb02dd0a34777443b8309ea4f98b
dev1 <dev@gmail.com>
2020-09-29 15:04:22 +0530

    kuna commited

--- SecondNewProject/src/com/nit/controller/SecondProject.java
+++ SecondNewProject/src/com/nit/controller/SecondProject.java
@@ -3,6 +3,7 @@ package com.nit.controller;
 public class SecondProject {
 	public static void main(String[] args) {
 		System.out.println("The main project");
+		System.out.println("Kuna is grand gandu");
 	}
 
 }
